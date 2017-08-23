774310c0e5ef4f90a3087b191a526e24539ac629
Manan12 <manan@mokko.io>
2017-08-23 12:51:20 +0530

    Added the List Page
    Now you can Add the basic List Item view by just one Tag  specifying the
    name and image prop.

--- src/App.js
+++ src/App.js
@@ -6,11 +6,14 @@ import Minus from 'react-icons/lib/fa/minus';
 import Square from 'react-icons/lib/fa/square-o';
 import Map from 'react-icons/lib/fa/map-o';
 import AreaChart from 'react-icons/lib/fa/area-chart';
+import ListIcon from 'react-icons/lib/fa/list';
 
 import Cards from './pages/Cards';
 import Buttons from './pages/Buttons';
 import Theme from './pages/Theme';
 import Charts from './pages/Charts';
+import List from './pages/List';
+
 
 class App extends Component {
   render() {
@@ -26,6 +29,7 @@ class App extends Component {
           <SideBarLink><NavLink to='/cards'><Square />Cards</NavLink></SideBarLink>
           <SideBarLink><NavLink to='/buttons'><Minus />Buttons</NavLink></SideBarLink>
           <SideBarLink><NavLink to='/charts'><AreaChart />Charts</NavLink></SideBarLink>
+          <SideBarLink><NavLink to='/list'><ListIcon />List</NavLink></SideBarLink>
 
         </SideBar>
         <MainPanel style={{background: '#F9F9F9', padding: '10px'}}>
@@ -33,6 +37,8 @@ class App extends Component {
             <Route path='/cards' component={Cards} />
             <Route path='/buttons' component={Buttons} />
             <Route path='/charts' component={Charts} />
+            <Route path='/list' component={List} />
+
         </MainPanel>
         </div>
       </Router>
