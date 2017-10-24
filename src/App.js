cab4bfc6c1fea829810bc57018c0ed39c67bbd3e
Devang Patel <devang@mokko.io>
2017-10-24 17:39:27 +0530

    Added theme provider wrapper

--- src/App.js
+++ src/App.js
@@ -21,35 +21,37 @@ const myTheme = makeTheme(theme);
 class App extends Component {
   render() {
     return (
-      <Router>
-          <div>
-            <SideBar shadow={true} activeBackground>
-              <Panel alignment="center">
-                <Padded margin="40px 0px 30px 0px">
-                  <img src={require('./images/logo.svg')} width="140" alt="" />
-                </Padded>
-              </Panel>
-              <SideBarLink><NavLink exact to='/'>Theme</NavLink></SideBarLink>
-              <SideBarLink><NavLink to='/charts'>Charts</NavLink></SideBarLink>
-              <SideBarLink><NavLink to='/cards'>Cards</NavLink></SideBarLink>
-              <SideBarLink><NavLink to='/lists'>Lists</NavLink></SideBarLink>
-              <SideBarLink><NavLink to='/tables'>Tables</NavLink></SideBarLink>
-              <SideBarLink><NavLink to='/buttons'>Buttons</NavLink></SideBarLink>
-              <SideBarLink><NavLink to='/form'>Forms</NavLink></SideBarLink>
-            </SideBar>
+      <ThemeProvider theme={myTheme}>
+        <Router>
+            <div>
+              <SideBar shadow={true} activeBackground>
+                <Panel alignment="center">
+                  <Padded margin="40px 0px 30px 0px">
+                    <img src={require('./images/logo.svg')} width="140" alt="" />
+                  </Padded>
+                </Panel>
+                <SideBarLink><NavLink exact to='/'>Theme</NavLink></SideBarLink>
+                <SideBarLink><NavLink to='/charts'>Charts</NavLink></SideBarLink>
+                <SideBarLink><NavLink to='/cards'>Cards</NavLink></SideBarLink>
+                <SideBarLink><NavLink to='/lists'>Lists</NavLink></SideBarLink>
+                <SideBarLink><NavLink to='/tables'>Tables</NavLink></SideBarLink>
+                <SideBarLink><NavLink to='/buttons'>Buttons</NavLink></SideBarLink>
+                <SideBarLink><NavLink to='/form'>Forms</NavLink></SideBarLink>
+              </SideBar>
 
-            <MainPanel backgroundColor="#F9F9F9">
-                <Route exact path='/' component={Theme} />
-                <Route path='/cards' component={Cards} />
-                <Route path='/charts' component={Charts} />
-                <Route path='/lists' component={Lists} />
-                <Route path='/tables' component={Tables} />
-                <Route path='/buttons' component={Buttons} />
-                <Route path='/form' component={Form} />
-            </MainPanel>
+              <MainPanel backgroundColor="#F9F9F9">
+                  <Route exact path='/' component={Theme} />
+                  <Route path='/cards' component={Cards} />
+                  <Route path='/charts' component={Charts} />
+                  <Route path='/lists' component={Lists} />
+                  <Route path='/tables' component={Tables} />
+                  <Route path='/buttons' component={Buttons} />
+                  <Route path='/form' component={Form} />
+              </MainPanel>
 
-          </div>
-      </Router>
+            </div>
+        </Router>
+      </ThemeProvider>
     );
   }
 }
